fd7f587e745e4aa3a7fc4037254add1af81239c0
Gabriela-LC <gabrielacavalcanti6@gmail.com>
2022-05-19 11:07:50 -0300

    modal para aviso de email ja cadastrado feito

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -13,6 +13,29 @@ async function coletaDados(event) {
   if (statusCadastro.id) {
     window.location = "./src/pages/login.html";
   } else {
+    modalAvisoCadastro()
     console.error(statusCadastro);
   }
+}
+
+function modalAvisoCadastro(){
+
+  const divModal = document.createElement("div")
+  divModal.classList.add("divModal")
+
+  const tituloModal = document.createElement("h3")
+  tituloModal.innerText = "Atenção!"
+
+  const textoModal = document.createElement("p")
+  textoModal.innerText = "O email preenchido já foi cadastrado anteriormente. Favor utilizar um novo email."
+
+  const btnFecharModal = document.createElement("button")
+  btnFecharModal.innerText = "Fechar"
+  btnFecharModal.addEventListener("click", () => {
+      divModal.remove()
+  })
+
+  divModal.append(tituloModal,textoModal,btnFecharModal)
+  document.body.append(divModal)
+
 }
